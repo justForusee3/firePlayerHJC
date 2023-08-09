55c62e48b91537cb6ef4af640a89dff949eeb089
vishwas <vishwas.tarende@ltts.com>
2023-08-09 11:43:29 +0530

    hello

---
+++ urls.js
@@ -0,0 +1,8 @@
+var urls = [
+    { name:"HMM", url: "https://assets.afcdn.com/video49/20210722/v_645516.m3u8", useDefaultDrmConfig: false },
+    // { name:"Envivo (DASH)", url: "https://dash.akamaized.net/envivio/EnvivioDash3/manifest.mpd", useDefaultDrmConfig: false },
+    // { name:"Sintel (DASH)", url: "http://amssamples.streaming.mediaservices.windows.net/d444b1c9-c64a-4d65-b0f4-433810cf5e92/SintelMultiAudio.ism/manifest(format=mpd-time-csf)", useDefaultDrmConfig: false },
+    // { name:"DASH Test Content", url: "https://cpetestutility.stb.r53.xcal.tv/VideoTestStream/main.mpd", useDefaultDrmConfig: false },
+	// { name:"HLS Test Content", url: "https://cpetestutility.stb.r53.xcal.tv/VideoTestStream/main.m3u8", useDefaultDrmConfig: false },
+    // { name:"Frag-MP4 Test Content", url: "https://cpetestutility.stb.r53.xcal.tv/VideoTestStream/main_mp4.m3u8", useDefaultDrmConfig: false }
+ ];
